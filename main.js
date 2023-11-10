4ad7d101f21b3ee9755de83db82a73126938e476
simonpizevski <78857703+simonpizevski@users.noreply.github.com>
2023-11-10 17:16:15 +0100

    mobile klar

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
 // My Javascript code
+document.getElementById("btnOpen").addEventListener("click", function()
+{
+    let containerMobilemenu = document.getElementById("contMM");
+    let bgMobilemenu = document.getElementById("bgMM");
+    
+    containerMobilemenu.style.display = "block";
+    bgMobilemenu.style.display = "flex";
+    console.log("hello")
+
+    document.querySelector("html").style.overflow = "hidden";
+})
+
+document.getElementById("btnClose").addEventListener("click", function()
+{
+    let containerMobilemenu = document.getElementById("contMM");
+    let bgMobilemenu = document.getElementById("bgMM");
+    
+    containerMobilemenu.style.display = "none";
+    bgMobilemenu.style.display = "none";
+    console.log("hello")
+
+    document.querySelector("html").style.overflow = "auto";
+})
+    
+
+
+
